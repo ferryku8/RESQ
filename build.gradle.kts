125480c9fbbaa92b3f605ada5f6a3bb61af62b3a
ferryku8 <ferryku8@gmail.com>
2026-02-12 16:52:15 +0700

    fix error

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
